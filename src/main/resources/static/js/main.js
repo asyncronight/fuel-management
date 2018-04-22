9ef60751d306002de11cfddf490cb899eeefc280
salah <salah.loukili@gmail.com>
2018-04-22 13:10:06 +0100

    Externalize errors and attentions messages.

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.js-example-basic-single').select2({
+        width: '100%'
+    });
+    $('body').bootstrapMaterialDesign();
+    $('[data-toggle="datepicker"]').datepicker({
+        format: "yyyy-mm-dd",
+        autoHide: true
+    });
+});
