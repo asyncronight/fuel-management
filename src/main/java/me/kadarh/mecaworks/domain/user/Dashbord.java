52d2b1154f3abc8287ef5c531364112a860c60af
salah <salah.loukili@gmail.com>
2018-05-25 01:51:34 +0000

    Add onglet 1 in gasoil dashboard
    Signed-off-by: salah <salah.loukili@gmail.com>

--- src/main/java/me/kadarh/mecaworks/domain/user/Dashbord.java
+++ src/main/java/me/kadarh/mecaworks/domain/user/Dashbord.java
@@ -2,12 +2,50 @@ package me.kadarh.mecaworks.domain.user;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class Dashbord {
 
-    private List<ChantierBatch> chantierBatch;
-    private List<Quantite> quantites;
+	private List<ChantierBatch> chantierBatch = new ArrayList<>();
+	private List<Quantite> quantites = new ArrayList<>();
 
+	//todo exclude last month (because there is 13 month in quantites) or just fill 12 month
+	// todo add prix to Quantite class
+	public Long getTotaleGazoile() {
+		return quantites.stream().mapToLong(Quantite::getQuantity).sum();
+	}
+
+	public Long getTotaleGazoileDh() {
+		return quantites.stream().mapToLong(quantite -> quantite.getQuantity() * 10).sum();
+	}
+
+	public Long getTotaleGazoileLocation() {
+		return quantites.stream().mapToLong(Quantite::getQuantiteLocation).sum();
+	}
+
+	public Long getTotaleGazoileLocationDh() {
+		return quantites.stream().mapToLong(quantite -> quantite.getQuantiteLocation() * 10).sum();
+	}
+
+	public Long getTotaleGazoileInterne() {
+		return quantites.stream().mapToLong(q -> q.getQuantity() - q.getQuantiteLocation()).sum();
+	}
+
+	public Long getTotaleGazoileInterneDh() {
+		return quantites.stream().mapToLong(q -> (q.getQuantity() - q.getQuantiteLocation()) * 10).sum();
+	}
+
+	public Long getChargeLocataireInterne() {
+		return quantites.stream().mapToLong(Quantite::getChargeLocataire).sum();
+	}
+
+	public Long getChargeLocataireExterne() {
+		return quantites.stream().mapToLong(Quantite::getChargeLocataireExterne).sum();
+	}
+
+	public Long getChargeLocataireTotale() {
+		return quantites.stream().mapToLong(quantite -> quantite.getChargeLocataire() + quantite.getChargeLocataireExterne()).sum();
+	}
 }
--- src/main/resources/static/js/user/gazoilHome.js
+++ src/main/resources/static/js/user/gazoilHome.js
@@ -1,1 +1,51 @@
-console.log(data);
+//console.log(data);
+var mixedChart = new Chart('chart1', {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: "Consommation gasoil totale (L)",
+            backgroundColor: 'rgb(66, 134, 244,0.2)',
+            borderColor: 'rgb(66, 134, 244)',
+            borderWidth: 1,
+            data: data.quantites.map(function (value) {
+                return value.quantity;
+            })
+        }, {
+            label: 'Consommation gasoil location (L)',
+            backgroundColor: 'rgb(249, 140, 62, 0.2)',
+            borderColor: 'rgb(249, 140, 62)',
+            borderWidth: 1,
+            data: data.quantites.map(function (value) {
+                return value.quantiteLocation;
+            })
+
+        }],
+        labels: data.quantites.map(function (value) {
+            return value.date;
+        })
+    }
+});
+var ctx2 = document.getElementById('chart2').getContext('2d');
+var mixedChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        datasets: [{
+            label: 'Charge locataire interne (DH)',
+            backgroundColor: 'rgb(59, 237, 83, 0.2)',
+            borderColor: 'rgb(59, 237, 83)',
+            data: data.quantites.map(function (value) {
+                return value.chargeLocataire;
+            })
+        }, {
+            label: 'Charge locataire externe (DH)',
+            backgroundColor: 'rgb(242, 48, 138, 0.2)',
+            borderColor: 'rgb(242, 48, 138)',
+            data: data.quantites.map(function (value) {
+                return value.chargeLocataireExterne;
+            })
+        }],
+        labels: data.quantites.map(function (value) {
+            return value.date;
+        })
+    }
+});
