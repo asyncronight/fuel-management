961c5ef20583e0cb3c32e0da512f51feb55e2f43
salah <salah.loukili@gmail.com>
2018-05-26 19:40:11 +0000

    Add 'consommation preuve' in first chart in onglet 1
    Signed-off-by: salah <salah.loukili@gmail.com>

--- src/main/resources/static/js/user/gazoilHome.js
+++ src/main/resources/static/js/user/gazoilHome.js
@@ -1,14 +1,27 @@
-//console.log(data);
+var range = "(" + data.quantites[1].date + " - " + data.quantites[12].date + ")";
+document.getElementById('range').innerText = range;
 var mixedChart = new Chart('chart1', {
     type: 'bar',
     data: {
         datasets: [{
             label: "Consommation gasoil totale (L)",
-            backgroundColor: 'rgb(66, 134, 244,0.2)',
-            borderColor: 'rgb(66, 134, 244)',
+            backgroundColor: 'rgb(7, 102, 255,0.2)',
+            borderColor: data.quantites.map(function (value) {
+                return parseInt(value.quantity, 10) > parseInt(value.consommationPrevue, 10) ? 'rgb(255, 0, 76)' : 'rgb(76, 220, 0)';
+            }),
             borderWidth: 1,
             data: data.quantites.map(function (value) {
                 return value.quantity;
+            })
+        }, {
+            label: "Consommation gasoil prevue (L)",
+            backgroundColor: 'rgb(0, 212, 255)',
+            borderColor: 'rgb(0, 212, 255)',
+            type: 'line',
+            fill: false,
+            borderWidth: 1,
+            data: data.quantites.map(function (value) {
+                return value.consommationPrevue;
             })
         }, {
             label: 'Consommation gasoil location (L)',
@@ -23,6 +36,27 @@ var mixedChart = new Chart('chart1', {
         labels: data.quantites.map(function (value) {
             return value.date;
         })
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Consommation gasoil en litre'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        hover: {
+            mode: 'index',
+            intersect: false
+        }
     }
 });
 var ctx2 = document.getElementById('chart2').getContext('2d');
@@ -31,15 +65,17 @@ var mixedChart2 = new Chart(ctx2, {
     data: {
         datasets: [{
             label: 'Charge locataire interne (DH)',
-            backgroundColor: 'rgb(59, 237, 83, 0.2)',
-            borderColor: 'rgb(59, 237, 83)',
+            backgroundColor: 'rgb(43, 106, 206, 0.1)',
+            borderColor: 'rgb(43, 106, 206)',
+            borderWidth: 2,
             data: data.quantites.map(function (value) {
                 return value.chargeLocataire;
             })
         }, {
             label: 'Charge locataire externe (DH)',
-            backgroundColor: 'rgb(242, 48, 138, 0.2)',
+            backgroundColor: 'rgb(242, 48, 138, 0.1)',
             borderColor: 'rgb(242, 48, 138)',
+            borderWidth: 2,
             data: data.quantites.map(function (value) {
                 return value.chargeLocataireExterne;
             })
@@ -47,5 +83,26 @@ var mixedChart2 = new Chart(ctx2, {
         labels: data.quantites.map(function (value) {
             return value.date;
         })
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Charge locataire en dirham'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        hover: {
+            mode: 'index',
+            intersect: false
+        }
     }
 });
