508bbe84450d8d1125ae5f4490414f9d70d3a932
salah <salah.loukili@gmail.com>
2018-05-26 19:37:58 +0000

    fix #7
    Signed-off-by: salah <salah.loukili@gmail.com>

--- src/main/java/me/kadarh/mecaworks/domain/user/Quantite.java
+++ src/main/java/me/kadarh/mecaworks/domain/user/Quantite.java
@@ -11,6 +11,8 @@ public class Quantite {
     private Long chargeLocataire = 0L;
     private Long chargeLocataireExterne = 0L;
     private Double consommationPrevue = 0.2D;
+	//todo : fill with real value
+	private Float prix = 10F;
 
     public Quantite(String date, Long quantity, Long quantiteLocation, Long chargeLocataire, Long chargeLocataireExterne, Double consommationPrevue) {
         this.date = date;
