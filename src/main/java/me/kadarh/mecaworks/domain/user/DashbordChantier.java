0ae9fe44556ebdaa9f93de8d732e1952e5d7ee67
kad <kadar.hamza@gmail.com>
2018-05-30 01:31:09 +0000

    - Fix #12 -

---
+++ src/main/java/me/kadarh/mecaworks/domain/user/DashbordChantier.java
@@ -0,0 +1,14 @@
+package me.kadarh.mecaworks.domain.user;
+
+import lombok.Data;
+
+/**
+ * PROJECT mecaworks
+ *
+ * @author kadarH
+ * Created at 30/05/18
+ */
+@Data
+public class DashbordChantier {
+
+}
