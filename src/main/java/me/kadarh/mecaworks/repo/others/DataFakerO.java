cdf3044668d9aa6f6aac5f66375fa659ae2be211
salah3x <salah.loukili@gmail.com>
2018-07-18 15:04:38 +0100

    fix #24 move data table & loader scripts from footer fragment to bons' template
    Signed-off-by: salah3x <salah.loukili@gmail.com>

--- src/main/java/me/kadarh/mecaworks/repo/others/DataFakerO.java
+++ src/main/java/me/kadarh/mecaworks/repo/others/DataFakerO.java
@@ -87,7 +87,6 @@ public class DataFakerO {
         userRepo.save(new User("user", encoder.encode("user"), Arrays.asList(AuthoritiesConstants.USER)));
         userRepo.save(new User("saisi", encoder.encode("saisi"), Arrays.asList(AuthoritiesConstants.SAISI)));
         userRepo.save(new User("admin", encoder.encode("admin"), Arrays.asList(AuthoritiesConstants.ADMIN)));
-        userRepo.save(new User("mecaworks", encoder.encode("mecaworks"), Arrays.asList(AuthoritiesConstants.ADMIN, AuthoritiesConstants.SAISI, AuthoritiesConstants.USER)));
     }
 
     private void loadGroupe(int n) {
