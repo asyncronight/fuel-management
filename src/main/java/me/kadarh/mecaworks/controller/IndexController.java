5773cdda9d70dd91716d1cb3b9f04c755634ce86
kad <kadar.hamza@gmail.com>
2018-10-15 23:05:27 +0100

    Add onglet Formation

--- src/main/java/me/kadarh/mecaworks/controller/IndexController.java
+++ src/main/java/me/kadarh/mecaworks/controller/IndexController.java
@@ -21,4 +21,9 @@ public class IndexController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("formation")
+    public String formation() {
+        return "formation/home";
+    }
 }
